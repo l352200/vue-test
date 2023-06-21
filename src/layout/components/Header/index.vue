f87fffec77778bc806170f0815f815edd449c3d4
lzh <linzhaohan@fjzzhy.cn>
2023-06-21 14:51:20 +0800

    搜索栏调整

--- src/layout/components/Header/index.vue
+++ src/layout/components/Header/index.vue
@@ -2,61 +2,59 @@
   <div class="app-header-wrapper">
     <div class="header-container">
       <div class="header-inner">
-        <img @click="handleJump('/home')" class="logo-item" :src="logo" alt="测试项目">
-        <div class="text-item">富强 · 民主 · 文明 · 河蟹 · 自由 · 平等 · 公正 · 法制 · 爱国 · 敬业 · 诚信 · 友善</div>
-        <div class="search-item">
-          <el-input class="" v-model="searchItem" placeholder="请输入搜索关键词" @click="handleSearch"></el-input>
+        <img @click="handleJump('/home')" class="logo-item" :src="logo" alt="测试项目" />
+        <div class="text-item">
+          心灵鸡汤可以护养心灵,是一种对心灵有帮助的东西。
         </div>
-      <div v-if="userInfo.username" class="img-con">
-        <img @click="handleJump('/personal/management/index')" class="user-img" :src="userImg" alt="">
-        <div>欢迎你，{{userInfo.username}}</div>
-        <div class="exit" @click="handleLogOut">退出</div>
+        <div class="search-item">
+          <el-input class="" v-model="searchItem" placeholder="请输入搜索关键词">
+            <el-button slot="append" icon="el-icon-search" @click="handleSearch"></el-button>
+          </el-input>
+        </div>
+        <div v-if="userInfo.username" class="img-con">
+          <img @click="handleJump('/personal/management/index')" class="user-img" :src="userImg" alt="" />
+          <div>欢迎你，{{ userInfo.username }}</div>
+          <div class="exit" @click="handleLogOut">退出</div>
+        </div>
+        <el-button v-if="!userInfo.username" class="login-btn" type="primary" round @click="handleJump('/login')">登录/注册</el-button>
       </div>
-      <!-- <div class="visitor">
-        <div class="visitor-text" @click="handleJumpLogin">登录/注册</div>
-      </div> -->
-      <el-button v-if="!userInfo.username" class="login-btn" type="primary" round @click="handleJump('/login')">登录/注册</el-button>
-      </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import logo from '../../../assets/images/logo.jpg'
-import userImg from '../../../assets/images/user.png'
+import { mapGetters } from "vuex";
+import logo from "../../../assets/images/logo.jpg";
+import userImg from "../../../assets/images/user.png";
 export default {
-  computed:{
-    ...mapGetters(['userInfo'])
+  computed: {
+    ...mapGetters(["userInfo"]),
   },
   data() {
     return {
       searchItem: "",
       userImg,
       logo,
-      isLogin: false
+      isLogin: false,
     };
   },
-  created() {
-
-  },
+  created() { },
   methods: {
     // 搜索
     handleSearch() {
-
+      this.$router.push('/info/search?keyword=' + this.searchItem)
     },
     //跳转登录
-    handleJump(url){
-      this.$router.push(url)
+    handleJump(url) {
+      this.$router.push(url);
     },
     //退出登录
-    handleLogOut(){
-      localStorage.removeItem('loginUser')
-      this.$store.dispatch('user/logOut')
+    handleLogOut() {
+      localStorage.removeItem("loginUser");
+      this.$store.dispatch("user/logOut");
       // this.$forceUpdate();
     },
-  }
+  },
 };
 </script>
 
@@ -109,31 +107,15 @@ export default {
         height: 40px;
         cursor: pointer;
       }
-      .exit{
+      .exit {
         color: #409eff;
         font-size: 14px;
         cursor: pointer;
       }
     }
-    // .visitor {
-    //   // display: flex;
-    //   // flex-flow: row nowrap;
-    //   // align-items: center;
-    //   width: 132px;
-    //   height: 40px;
-    //   background: rgb(30, 108, 190);
-    //   border-radius: 20px;
-    //   color: #fff;
-    //   text-align: center;
-    //   // margin-bottom: 100px;
-    //   .visitor-text {
-    //     padding-top: 8px;
-    //   }
-    // }
-    .login-btn{
+    .login-btn {
       // display: block;
       margin-left: 100px;
-
     }
   }
 }
--- src/views/info/search.vue
+++ src/views/info/search.vue
@@ -3,55 +3,89 @@
     <div class="info-search-con">
       <div class="info-search-head">
         <div class="inp">
-          <el-radio-group style="width:300px" v-model="searchType">
+          <el-radio-group style="width: 300px" v-model="searchType">
             <el-radio :label="1">资讯</el-radio>
             <el-radio :label="2">事项</el-radio>
           </el-radio-group>
-          <el-input placeholder="请输入搜索关键词" v-model="keyword" clearable></el-input>
-          <el-button type="primary" size="small" @click="handleSearch"><i class="el-icon-search"></i> 搜索</el-button>
+          <el-input
+            placeholder="请输入搜索关键词"
+            v-model="keyword"
+            clearable
+          ></el-input>
+          <el-button
+            style="margin-left: 20px"
+            size="small"
+            type="primary"
+            @click="handleSearch"
+            icon="el-icon-search"
+            >搜索</el-button
+          >
         </div>
       </div>
-      <div class="info-search-list">
-        <el-card class="card" v-for="item in list" :key="item.id" @click.native="item.type==1?handleJumpNews(item.id):handleJump(item.content)">
+      <div v-if="list.length" class="info-search-list">
+        <el-card
+          class="card"
+          v-for="item in list"
+          :key="item.id"
+          @click.native="
+            item.type == 1 ? handleJumpNews(item.id) : handleJump(item.content)
+          "
+        >
           <div>
-            {{item.title}}
+            {{ item.title }}
           </div>
         </el-card>
       </div>
+      <div class="nodata-con" v-else>
+        <img class="nodata-img" :src="nodata" alt="" />
+      </div>
       <div style="text-align: right">
-        <el-pagination :current-page="pageNum" :page-size="pageSize" :page-sizes="[5, 10, 20]" :total="total" layout="total, sizes, prev, pager, next, jumper"
-                       @current-change="handleCurrentChange" @size-change="handleSizeChange"></el-pagination>
+        <el-pagination
+          :current-page="pageNum"
+          :page-size="pageSize"
+          :page-sizes="[5, 10, 20]"
+          :total="total"
+          layout="total, sizes, prev, pager, next, jumper"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        ></el-pagination>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import nodata from "@/assets/images/nodata.png";
 export default {
   data() {
     return {
-      keyword: '',
+      keyword: "",
       list: [],
       total: 0,
       pageNum: 1,
       pageSize: 5,
-      searchType: 1
-    }
+      searchType: 1,
+      nodata,
+    };
   },
   created() {
-    this.handleSearch()
+    console.log(this.$route.query);
+    if (this.$route.query.keyword) {
+      this.keyword = this.$route.query.keyword;
+    }
+    this.handleSearch();
   },
   methods: {
     //搜索
     handleSearch() {
-      this.pageNum = 1
-      this.handleLoadData()
+      this.pageNum = 1;
+      this.handleLoadData();
     },
     handleLoadData() {
-      if(this.searchType == 1) {
-        this.getNews()
-      } else if(this.searchType == 2) {
-        this.getMatters()
+      if (this.searchType == 1) {
+        this.getNews();
+      } else if (this.searchType == 2) {
+        this.getMatters();
       }
     },
     getNews() {
@@ -61,11 +95,11 @@ export default {
         data: {
           keyword: this.keyword,
           pageNum: this.pageNum,
-          pageSize: this.pageSize
+          pageSize: this.pageSize,
         },
         success: (res) => {
-          this.list = res.rows
-          this.total = res.count
+          this.list = res.rows;
+          this.total = res.count;
         },
         error: (res) => {
           this.$alert(res, "res");
@@ -79,11 +113,11 @@ export default {
         data: {
           keyword: this.keyword,
           pageNum: this.pageNum,
-          pageSize: this.pageSize
+          pageSize: this.pageSize,
         },
         success: (res) => {
-          this.list = res.rows
-          this.total = res.count
+          this.list = res.rows;
+          this.total = res.count;
         },
         error: (res) => {
           this.$alert(res, "res");
@@ -93,13 +127,13 @@ export default {
     //跳转单条咨询页
     handleJumpNews(id) {
       console.log(123);
-      window.open(location.origin + '/#/page/info/' + id + '/1')
+      window.open(location.origin + "/#/page/info/" + id + "/1");
     },
     handleJump(url) {
-      if(url.indexOf('https') == -1) {
-        url = 'https://' + url
+      if (url.indexOf("https") == -1) {
+        url = "https://" + url;
       }
-      window.open(url)
+      window.open(url);
     },
     // 页码变化
     handleCurrentChange(value) {
@@ -112,8 +146,8 @@ export default {
       this.pageSize = value;
       this.handleLoadData();
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -136,6 +170,16 @@ export default {
         margin-bottom: 20px;
         padding: 5px;
         cursor: pointer;
+      }
+    }
+    .nodata-con {
+      width: 100%;
+      height: 300px;
+      text-align: center;
+      .nodata-img {
+        width: 400px;
+        height:300px;
+        margin: 0 auto;
       }
     }
   }
